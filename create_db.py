98be5a6ffd7e2af427fc856576569f9345a7a1fb
Devesh Nischal <devesh.nischal@gmail.com>
2020-10-30 00:39:39 -0400

    Formatted code using flake8 and black.

--- create_db.py
+++ create_db.py
@@ -1,6 +1,5 @@
 # create_db.py
 from project.app import db
-from project.models import Post
 
 # create the database and the db table
 db.create_all()
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,10 +1,11 @@
 import pytest
-import os
 import json
 from pathlib import Path
 from project.app import app, db
 
+
 TEST_DB = "test.db"
+
 
 @pytest.fixture
 def client():
@@ -17,6 +18,7 @@ def client():
     yield app.test_client()  # tests run here
     db.drop_all()  # teardown
 
+
 def login(client, username, password):
     """Login helper function"""
     return client.post(
@@ -25,23 +27,28 @@ def login(client, username, password):
         follow_redirects=True,
     )
 
+
 def logout(client):
     """Logout helper function"""
     return client.get("/logout", follow_redirects=True)
 
+
 def test_index(client):
     response = client.get("/", content_type="html/text")
     assert response.status_code == 200
+
 
 def test_database(client):
     """initial test. ensure that the database exists"""
     tester = Path("test.db").is_file()
     assert tester
 
+
 def test_empty_db(client):
     """Ensure database is blank"""
     rv = client.get("/")
     assert b"No entries yet. Add some!" in rv.data
+
 
 def test_login_logout(client):
     """Test login and logout using helper functions"""
@@ -53,6 +60,7 @@ def test_login_logout(client):
     assert b"Invalid username" in rv.data
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
     assert b"Invalid password" in rv.data
+
 
 def test_messages(client):
     """Ensure that user can post messages"""
@@ -66,6 +74,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
@@ -76,9 +85,10 @@ def test_delete_message(client):
     data = json.loads(rv.data)
     assert data["status"] == 1
 
+
 def test_search(client):
     """Ensure the posts can be searched"""
-    #add a dummy post to be searched
+    # add a dummy post to be searched
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
     client.post(
         "/add",
@@ -86,12 +96,12 @@ def test_search(client):
         follow_redirects=True,
     )
 
-    #search for that post
+    # search for that post
     rv = client.get("/search/?query=test")
     assert b"Test" in rv.data
     assert b"Test post" in rv.data
 
-    #search for different post
+    # search for different post
     rv = client.get("/search/?query=random")
     assert b"Test" not in rv.data
     assert b"Test post" not in rv.data
